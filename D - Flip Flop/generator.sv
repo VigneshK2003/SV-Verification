fea15c64ea9065db885156fad0eb91600b17c8b5
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-01-07 23:39:58 +0530

    Create generator.sv

---
+++ D - Flip Flop/generator.sv
@@ -0,0 +1,21 @@
+// generator class
+
+class generator;
+  
+   transaction tr;
+   
+  mailbox gen_driv;
+  
+  function new(mailbox gen_driv);
+       this.gen_driv = gen_driv;
+  endfunction
+  
+  task gen_task();
+    repeat(5) begin
+    tr = new();
+    tr.randomize();
+      gen_driv.put(tr);
+      tr.display("---generator signals---");
+    end
+  endtask
+endclass
