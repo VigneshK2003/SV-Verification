8f976a934c9f247bda63121fb6c368d167b1da6c
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-01-08 10:21:56 +0530

    Create environment.sv

---
+++ D - Flip Flop/environment.sv
@@ -0,0 +1,42 @@
+
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
+class environment;
+  generator gen;
+  driver driv;
+  monitor mon;
+  scoreboard scb;
+  
+  mailbox gen_driv;
+  mailbox mon_scb;
+  
+  virtual intf_df vif;
+  
+  function new( virtual intf_df vif_tb);
+     vif = vif_tb;
+    
+    gen_driv = new();
+    mon_scb = new();
+    
+    gen = new(gen_driv);
+    driv = new(gen_driv,vif);
+    mon = new(mon_scb,vif);
+    scb = new(mon_scb);
+    
+  endfunction
+  
+  task drivedata();
+    fork
+      gen.gen_task();
+      driv.driv_task();
+      mon.mon_task();
+      scb.scb_task();
+    join
+  endtask
+endclass
+    
+    
